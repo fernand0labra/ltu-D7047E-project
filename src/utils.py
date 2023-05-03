613147ee5065cce91c98c6b2718c6ebd5c8df52c
Amirhossein Nayebiastaneh <amirhossein.nayebiastaneh@gmail.com>
2023-05-03 17:39:22 +0000

    Implements system RNN. Unsatisfactory results...

--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.tensorboard.writer import SummaryWriter
+from torch.utils.data import DataLoader
 
 
-def generate_random_steps(num_steps, max_step_height, max_signal_val, step_width):
+def generate_random_steps(num_steps, max_step_height, max_signal_val,
+                          step_width):
     """
     Generates a signal of random steps.
 
@@ -18,7 +20,8 @@ def generate_random_steps(num_steps, max_step_height, max_signal_val, step_width
     Returns:
         numpy.array: A 1D array representing the generated signal.
     """
-    signal = np.zeros(num_steps * step_width)  # Initialize the signal with zeros
+    signal = np.zeros(num_steps *
+                      step_width)  # Initialize the signal with zeros
     current_val = 0  # Start from 0
 
     for i in range(num_steps):
@@ -36,7 +39,10 @@ def generate_random_steps(num_steps, max_step_height, max_signal_val, step_width
 
 def save_signal_data(path, dataloader):
     with open(path, mode='w', newline='') as file:
-        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.writer(file,
+                            delimiter=',',
+                            quotechar='"',
+                            quoting=csv.QUOTE_MINIMAL)
 
         # write the header row
         writer.writerow(['input', 'output'])
@@ -52,86 +58,81 @@ def save_signal_data(path, dataloader):
             writer.writerow([input_column, output_column])
 
 
-def plot_signal_data(sys, net, device):
-    num_steps = 100
-    step_width = 100
-    max_step_height = 3
-    max_signal_val = 3.0
+def plot_signal_data(data_loader: DataLoader, sys, net: nn.Module, device):
+    input_signals, output_signals = next(iter(data_loader))
+    input_signal = torch.unsqueeze(torch.unsqueeze(input_signals[0], 0), -1).to(device)
+    output_signal = output_signals[0]
 
-    input_signal = generate_random_steps(num_steps, max_step_height, max_signal_val, step_width)
-    ts, output_signal = sys.run(input_signal, 0)
+    generated_signal, _ = net(input_signal)
 
-    u_tensor = torch.tensor(input_signal, dtype=torch.float32).unsqueeze(0).to(device)
-    generated_signal = net.forward(u_tensor)
-
+    ts = data_loader.dataset.dataset.ts
     # Plot the generated input signal
-    plt.plot(ts, input_signal)
+    plt.plot(ts, torch.squeeze(input_signal.cpu()))
     plt.xlabel('Time (s)')
     plt.ylabel('u(t)')
     plt.title('A Sample Input Signal for Identification')
     plt.show()
 
     # Plot the generated output signal
-    plt.plot(ts, output_signal)
+    plt.plot(ts, output_signal, label="Sys Output")
+    plt.plot(ts, torch.squeeze(generated_signal.detach().cpu()), label="Model Output")
     plt.xlabel('Time (s)')
     plt.ylabel('y(t)')
     plt.title('Van der Pol System Output')
-    plt.show()
-
-    # Plot the generated output signal
-    plt.plot(ts, generated_signal.detach().numpy())
-    plt.xlabel('Time (s)')
-    plt.ylabel('rnn(t)')
-    plt.title('RNN System Output')
+    plt.legend()
     plt.show()
 
 
-def train(epochs, trainloader, optimizer, criterion, net, device):
-    writer = SummaryWriter("../logs")
+def train(epochs, train_loader, val_loader, optimizer, criterion,
+          net: nn.Module, device):
+    writer = SummaryWriter("./logs")
 
     # Train the network
     for epoch in range(epochs):  # Loop over the dataset multiple times
+        net.train()
+        train_loss = 0.0
+        counter = 0
+        for batch_index, (inputs, ground_truths) in enumerate(train_loader, 0):
+            inputs = torch.unsqueeze(inputs, -1).to(device)
+            ground_truths = torch.unsqueeze(ground_truths, -1).to(device)
 
-        running_loss = 0.0
-        for i, (inputs, ground_truth) in enumerate(trainloader, 0):
-            inputs = inputs.to(device)
-            ground_truth = ground_truth[0].to(device)
-
-            # zero the parameter gradients
+            # zero the gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = net(inputs).to(device)  # GPU
-            loss = criterion(outputs, ground_truth)
-            writer.add_scalar("Loss/train", loss, epoch)
+            outputs, _ = net(inputs)
+            loss = criterion(outputs, ground_truths)
             loss.backward()
             optimizer.step()
 
-            # print statistics
-            running_loss += loss.item()
+            train_loss += loss.item()
+            counter += 1
 
-        print(f'[{epoch + 1}] loss: {running_loss / 1000:.3f}')
+        train_loss /= counter
+        val_loss = test(val_loader, criterion, net, device)
+
+        writer.add_scalar("Loss/train", train_loss, epoch)
+        writer.add_scalar("Loss/val", val_loss, epoch)
+
+        print(
+            f'[{int((epoch + 1.0) / epochs * 100)}%] train loss: {train_loss:.3f}, val loss: {val_loss:.3f}'
+        )
 
     writer.flush()
     writer.close()
 
 
-def test(testloader, net, device):
-    mse = 0
-    total = 0
-    loss = nn.MSELoss()
+def test(data_loader, criterion, net: nn.Module, device):
+    net.eval()
+    loss = 0
     # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
-        for (inputs, ground_truth) in testloader:
+        counter = 0
+        for (inputs, ground_truths) in data_loader:
+            inputs = torch.unsqueeze(inputs, -1).to(device)
+            ground_truths = torch.unsqueeze(ground_truths, -1).to(device)
+            outputs, _ = net(inputs)
+            loss += criterion(outputs, ground_truths)
+            counter += 1
 
-            inputs = inputs.to(device)
-            ground_truth = ground_truth[0].to(device)
-
-            # calculate outputs by running images through the network
-            outputs = net(inputs).to(device)
-            # the class with the highest energy is what we choose as prediction
-            total += inputs[0].size(0)
-            mse += loss(outputs, ground_truth)
-
-    print(f'Average MSE of the network on the 1000 signal points: {100 * mse / total:.3f}')
-
+    return loss / counter
