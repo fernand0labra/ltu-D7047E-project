c84856b959fa65dd6a37b34b9c74f4ebef385396
Amirhossein Nayebiastaneh <amirhossein.nayebiastaneh@gmail.com>
2023-05-14 17:58:19 +0000

    Clamps the control signal in JointRNN

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import torch
-a=torch.randn(2, 1, 3)
-print(a)
-print(a.dim())
-a = a.view(1, *a.shape)
-print(a.shape)
-print(a)
+
+x = torch.randn(3, 3, requires_grad=True)
+y = torch.clamp(x, 0, 1)
+z = y.sum()
+
+z.backward() # compute gradients
+
+print(x)
+print(x.grad) # prints gradients of x
