88a9ffba9e5f582096a24e324a1a8350888fbee5
Amirhossein Nayebiastaneh <amirhossein.nayebiastaneh@gmail.com>
2023-05-19 10:03:15 +0200

    ...
    Co-authored-by: Fernando Labra Caso <fernandolabracaso@gmail.com>

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-import torch
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
 
-x = torch.randn(3, 3, requires_grad=True)
-y = torch.clamp(x, 0, 1)
-z = y.sum()
+# Generate a random pulse train
+t = np.linspace(0, 1, 1000)  # Time axis
+pulse_train = np.random.choice([0, 1], size=len(t), p=[0.2, 0.8])  # Random pulse train
 
-z.backward() # compute gradients
+# Design a low-pass filter
+cutoff_freq = 10  # Cutoff frequency in Hz
+nyquist_freq = 0.5 * 1000  # Nyquist frequency (half the sampling rate)
+normalized_cutoff = cutoff_freq / nyquist_freq
+b, a = signal.butter(4, normalized_cutoff, btype='low', analog=False)
 
-print(x)
-print(x.grad) # prints gradients of x
+# Apply the low-pass filter
+filtered_signal = signal.lfilter(b, a, pulse_train)
+
+# Plot the original pulse train and the filtered signal
+plt.figure(figsize=(10, 6))
+plt.plot(t, pulse_train, label='Original')
+plt.plot(t, filtered_signal, label='Filtered')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.title('Original Pulse Train and Filtered Signal')
+plt.legend()
+plt.show()
